c6bbeac70c9442587efe8d1607cad625bc15d904
copev313 <copev313@gmail.com>
2022-09-17 22:26:48 -0400

    added proper coffee store dynamic pages w/ styling

---
+++ coffee-shop/components/Card.js
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import Image from "next/image"
+import cls from "classnames"
+
+import styles from "../styles/Card.module.css"
+
+const Card = (props) => {
+  return (
+    <div>
+      <Link href={props.href}>
+        <a className={styles.cardLink}>
+          <div className={cls("glass", styles.container)}>
+            <div className={styles.cardHeaderWrapper}>
+              <h2 className={styles.cardHeader}>{props.name}</h2>
+            </div>
+            <div className={styles.cardImageWrapper}>
+              <Image
+                className={styles.cardImage}
+                src={props.imgUrl}
+                width={260}
+                height={160}
+              />
+            </div>
+          </div>
+        </a>
+      </Link>
+    </div>
+  )
+}
+
+export default Card
--- coffee-shop/pages/coffee-store/[id].js
+++ coffee-shop/pages/coffee-store/[id].js
@@ -1,22 +1,116 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
+import { useRouter } from "next/router"
+import Head from "next/head"
+import Link from "next/link"
+import Image from "next/image"
+import cls from "classnames"
+import styles from "../../styles/coffee-store.module.css"
+import coffeeStoresData from "../../data/coffee-stores.json"
 
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params
 
-const CoffeeStore = () => {
-
-    const router = useRouter()
-    const { id } = router.query
-
-    return (
-        <div>
-            <Link href="/">
-                <a>Back to home</a>
-            </Link>
-
-            <h1>Coffee Store</h1>
-        </div>
-    )
+  return {
+    props: {
+      coffeeStores: coffeeStoresData.find((store) => {
+        return store.id.toString() === params.id
+      }),
+    }, // will be passed to the page component as props
+  }
 }
 
+export async function getStaticPaths() {
+  const paths = coffeeStoresData.map((store) => {
+    return {
+      params: {
+        id: store.id.toString(),
+      },
+    }
+  })
+
+  return {
+    paths: paths,
+    fallback: true,
+  }
+}
+
+const CoffeeStore = (props) => {
+  const router = useRouter()
+  const { id } = router.query
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  const { address, name, neighborhood, imgUrl } = props.coffeeStores
+
+  const handleUpvoteButton = () => {
+    console.log("Upvote button clicked!")
+  }
+
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+
+          <Image
+            src={imgUrl}
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/icons/places.svg"
+              width={24}
+              height={24}
+              alt="Location Icon"
+            />
+            <p className={styles.text}>{address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/icons/nearMe.svg"
+              width={24}
+              height={24}
+              alt="Neighvorhood Icon"
+            />
+            <p className={styles.text}>{neighborhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/icons/star.svg"
+              width={24}
+              height={24}
+              alt="Rating Icon"
+            />
+            <p className={styles.text}>{1}</p>
+          </div>
+
+          <button
+            className={styles.upvoteButton}
+            onclick={handleUpvoteButton}
+          >
+            Add to Favorites
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default CoffeeStore
